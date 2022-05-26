4ff51e7f6c8e5e570348977496bb23465b8a90d0
chrehall68 <hallchristopher8697@students.esuhsd.org>
2022-05-26 14:01:47 -0700

    add more tests

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -15,19 +15,28 @@ using namespace std;
         if (!static_cast<bool>(condition)) cerr << message << endl; \
         assert(condition);                                          \
     }
+
 // make sure that board.contains really works since we will be using it a lot
 void test_contains(const Board& board) {
-    for (int y = 0; y < 8; ++y) {
-        for (int x = 0; x < 8; ++x) {
+    for (int y = -10; y < 10; ++y) {
+        for (int x = -10; x < 10; ++x) {
             bool expected = x >= 0 && x < 8 && y >= 0 && y < 8;
-            assert(expected == board.contains(Cell(x, y)));
+            try {
+                ostringstream temp;
+                temp << "expected the board to contain " << Cell(x, y) << " but it didn't contain it";
+                assertm(expected == board.contains(Cell(x, y)), temp.str());
+            } catch (out_of_range e) {
+                ostringstream temp;
+                temp << "expected the board to not contain cell (" << x << ", " << y << ") but it did contain it";
+                assertm(!expected, temp.str());
+            }
         }
     }
 }
 
 // makes sure that regular moving (just swapping) works
 // tests occasions that will never happen as well
-void test_make_classical_move(Board& board) {
+void test_make_classical_chess_move(Board& board) {
     board.reset_board();
     for (int y = 0; y < 8; ++y) {
         for (int x = 0; x < 8; ++x) {
@@ -37,6 +46,74 @@ void test_make_classical_move(Board& board) {
             assert(board[Cell(4, 4)] == *there);
             board.reset_board();
         }
+    }
+}
+
+void test_make_moves(Board& board) {
+    // test when it is too small
+    for (int i = 1; i < 10; ++i) {
+        // to tests
+        bool threw_to_error = false;
+        int to_x = -(rand() % i + 1);
+        int to_y = -(rand() % i + 1);
+        try {
+            board.make_move(Move(Cell(0, 0), Cell(to_x, to_y)));  // this should throw an out of range error
+        } catch (exception e) {
+            threw_to_error = true;
+        }
+
+        ostringstream temp;
+        temp << "expected board.make_move to throw an error when trying to move to (" << to_x << ", " << to_y << "), but board.make_move didn't";
+        assertm(threw_to_error, temp.str());
+
+        // from tests
+        bool threw_from_error = false;
+        int from_x = -(rand() % i + 1);
+        int from_y = -(rand() % i + 1);
+
+        try {
+            board.make_move(Move(Cell(from_x, from_y), Cell(4, 4)));  // this should throw an out of range error
+        } catch (exception e) {
+            threw_from_error = true;
+        }
+
+        temp.str("");
+        temp.clear();
+        temp << "expected board.make_move to throw an error when trying to move from (" << from_x << ", " << from_y << "), but board.make_move didn't";
+        assertm(threw_from_error, temp.str());
+    }
+
+    // test when it is too large
+    for (int i = 1; i < 10; ++i) {
+        // to tests
+        bool threw_to_error = false;
+        int to_x = 9 + rand() % i;
+        int to_y = 9 + rand() % i;
+        try {
+            board.make_move(Move(Cell(0, 0), Cell(to_x, to_y)));  // this should throw an out of range error
+        } catch (out_of_range e) {
+            threw_to_error = true;
+        }
+
+        ostringstream temp;
+        temp << "expected board.make_move to throw an error when trying to move to (" << to_x << ", " << to_y << "), but board.make_move didn't";
+        assertm(threw_to_error, temp.str());
+
+        // from tests
+        bool threw_from_error = false;
+        int from_x = 9 + rand() % i;
+        int from_y = 9 + rand() % i;
+
+        try {
+            board.make_move(Move(Cell(to_x, to_y), Cell(4, 4)));  // this should throw an out of range error
+        } catch (out_of_range e) {
+            threw_from_error = true;
+        }
+
+        temp.str("");
+        temp.clear();
+        temp << "expected board.make_move to throw an error when trying to move from (" << to_x << ", " << to_y << "), but board.make_move didn't";
+        assertm(threw_from_error, temp.str());
     }
 }
 
@@ -55,7 +132,7 @@ ostream& operator<<(ostream& os, const vector<T> v) {
     return os << "}";
 }
 // make sure that all the moves returned follow the displacements set by 'directions'
-void test_correct_get_moves(const ChessPiece& piece, const Board& board, const Cell& cur_cell, const vector<Cell>& directions) {
+void test_get_moves(const ChessPiece& piece, const Board& board, const Cell& cur_cell, const vector<Cell>& directions) {
     vector<Move> moves;
     piece.get_moves(board, cur_cell, moves);
     vector<Cell> destinations;  // faster search times
@@ -84,10 +161,54 @@ void test_has_team_name(const ChessPiece& piece) {
     assert(temp != "UNKNOWN" && (piece == EMPTY_SPACE || temp != "None"));  // if it is unnamed (unless it's empty space), something is wrong
 }
 
+void test_winner(Board& board) {
+    for (int i = 0; i < 3; ++i) {
+        board.reset_board();
+
+        Team actual_winner = NONE;
+        bool found_white_king = false, found_black_king = false;
+        for (int y = 0; y < 8; ++y) {
+            for (int x = 0; x < 8; ++x) {
+                if (&board[Cell(x, y)] == &WHITE_KING) {
+                    if (i != 1) {
+                        found_white_king = true;
+                    } else {
+                        board.make_classical_chess_move(Move(Cell(4, 4), Cell(x, y)));  // kill the white king with an empty space
+                    }
+                } else if (&board[Cell(x, y)] == &BLACK_KING) {
+                    if (i != 2) {
+                        found_black_king = true;
+                    } else {
+                        board.make_classical_chess_move(Move(Cell(4, 4), Cell(x, y)));  // kill the black king with an empty space
+                    }
+                }
+            }
+        }
+        if (!found_white_king) {
+            actual_winner = BLACK;
+        }
+        if (!found_black_king) {
+            actual_winner = WHITE;
+        }
+
+        Team winner = board.winner();
+
+        ostringstream temp;
+        temp << "Expected the winner to be " << actual_winner << " but got " << winner;
+        assertm(actual_winner == winner, temp.str());
+    }
+}
+
 int main() {
     Board m;
     vector<Cell> pawn_moves = {Cell(0, 1)};
-    test_correct_get_moves(m[Cell(0, 1)], m, Cell(0, 1), pawn_moves);
+    test_get_moves(m[Cell(0, 1)], m, Cell(0, 1), pawn_moves);
+
+    test_contains(m);
+    test_make_classical_chess_move(m);
+    test_make_moves(m);
+
+    test_winner(m);
 
     cout << "all tests passed" << endl;
 }
